ed6a5fed46978b1a0c5a494fbd92139548f2988a
Pedro Augusto <pedro.sampaio@sps.ce.gov.br>
2025-06-16 15:56:35 -0300

    Seção 4: Novo módulo (module) e controller (controlador) para nossa aplicação NestJS

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,12 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('messages')
+export class MessagesController {
+  findAll() {
+    return null;
+  }
+
+  findOne() {
+    return null;
+  }
+}

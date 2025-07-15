f4a1a007898a880dea8e845ebfbe0c4dcef98c0f
Pedro Augusto <pedro.sampaio@sps.ce.gov.br>
2025-07-15 10:07:09 -0300

    Seção 5: CRUD de Pessoa - criando o Update

--- src/persons/persons.service.ts
+++ src/persons/persons.service.ts
@@ -50,8 +50,22 @@ export class PersonsService {
     return `This action returns a #${id} person`;
   }
 
-  update(id: number, updatePersonDto: UpdatePersonDto) {
-    return `This action updates a #${id} person`;
+  async update(id: number, updatePersonDto: UpdatePersonDto) {
+    const personData = {
+      name: updatePersonDto.name,
+      passwordHash: updatePersonDto.password,
+    };
+
+    const person = await this.personRepository.preload({
+      id,
+      ...personData,
+    });
+
+    if (!person) {
+      throw new NotFoundException('Pessoa não encontrada');
+    }
+
+    return this.personRepository.save(person);
   }
 
   async remove(id: number) {

eab8661676817bbcd23fef26a80b1f62d0854557
Pedro Augusto <pedro.sampaio@sps.ce.gov.br>
2025-07-16 11:25:54 -0300

    Seção 6: Interceptors - alterando os dados de resposta com map

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -8,29 +8,21 @@ import {
   Patch,
   Post,
   Query,
-  UseInterceptors,
 } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
 import { PaginationDto } from '../common/dto/pagination.dto';
-import { TimingConnectionInterceptor } from '../common/interceptors/timing-connection.interceptor';
-import { ErrorHandlingInterceptor } from '../common/interceptors/error-handling.interceptor';
-import { SimpleCacheInterceptor } from '../common/interceptors/simple-cache.interceptor';
-import { AddHeaderInterceptor } from '../common/interceptors/add-header.interceptor';
 
 @Controller('messages')
-@UseInterceptors(SimpleCacheInterceptor)
 export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
 
   @Get()
-  @UseInterceptors(TimingConnectionInterceptor)
   async findAll(@Query() paginationDto: PaginationDto) {
     return await this.messagesService.findAll(paginationDto);
   }
 
-  @UseInterceptors(AddHeaderInterceptor, ErrorHandlingInterceptor)
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.messagesService.findOne(id);
